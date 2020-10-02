5b6e7da89d7c93d49d7212fac26a827766cf0353
wandol <wandol@kakao.com>
2020-10-02 16:39:37 +0900

    init commit

---
+++ src/main/java/test/java/TestJavaApplication.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/java/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
